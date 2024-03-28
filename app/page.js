ef4fc15cdd4c9119f8484877dc9b78259e1cf647
DinosMpo <dinos7591@yahoo.gr>
2024-03-28 13:21:18 +0200

    refactor the desktop section component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
-import DesktopSection from './comps/DesktopSection/DesktopSection'
+"use client";
+
+import { useState } from 'react';
+import Section1 from './comps/Section1/Section1';
+import Section2 from './comps/Section2/Section2';
 
 export default function Home() {
+  const [stepIndicator, setStepIndicator] = useState(1);
+
   return (
     <div className='container'>
-      <DesktopSection />
+      <div className='section-wrapper'>
+        <Section1 stepIndicator={stepIndicator} />
+        <Section2 stepIndicator={stepIndicator} setStepIndicator={setStepIndicator} />
+      </div>
     </div>
   )
 }
